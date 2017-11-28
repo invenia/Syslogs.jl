487ce4d8523fb8a46db23023d56e13c7aa990eb8
Rory-Finnegan <rory.finnegan@gmail.com>
2017-11-28 17:29:44 -0600

    Initial pass providing local, UDP and TCP logging.

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 [![Build Status](https://travis-ci.org/invenia/SystemLog.jl.svg?branch=master)](https://travis-ci.org/invenia/SystemLog.jl)
 [![Build status](https://ci.appveyor.com/api/projects/status/github/invenia/SystemLog.jl?svg=true)](https://ci.appveyor.com/project/invenia/SystemLog-jl)
 [![codecov](https://codecov.io/gh/invenia/SystemLog.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/invenia/SystemLog.jl)
+
+## Features
+
+- Local logging with the syslog libc interface
+- Remote logging via UDP or TCP.
+- Julia interface to the basic libc calls
+- `Syslog` type which is a subtype for `IO` for use in other logging libraries (e.g., Memento.jl)
+
+## TODO
+
+- TLS support with MbedTLS.jl
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,89 @@
-using SystemLog
 using Base.Test
+using SystemLog
 
-# Write your own tests here.
-@test 1 == 2
+import Base.Libdl
+
+include("helpers.jl")
+
+@testset "Syslog" begin
+    @testset "Local" begin
+        info("Local Tests")
+        io = Syslog()
+        println(io, "info", "foobar")
+    end
+
+    @testset "Remote" begin
+        logs = filter(!isempty, split(readstring("scripts/output.log"), "\n\0"))
+        test_logs = map(logs) do s
+            input = replace(s, r"^<\d+>", "")
+            level = last(split(first(split(s, ':'))))
+            (level, input, s * '\0')
+        end
+
+        @testset "UDP" begin
+            info("UDP Tests")
+            @testset "Simple" begin
+                r = udp_srv(8080)
+                io = Syslog(ip"127.0.0.1", 8080; tcp=false)
+                println(io, "info", "foobar")
+                s = fetch(r)
+                @test contains(s, "foobar")
+                close(io)
+            end
+
+            @testset "SysLogHandler" begin
+                @testset "$input" for (level, input, output) in test_logs
+                    r = udp_srv(8080)
+                    io = Syslog(ip"127.0.0.1", 8080; tcp=false)
+                    info("Sending (UDP): $level, $input")
+                    println(io, level, input)
+                    s = fetch(r)
+                    @test strip(s) == strip(output)
+                    close(io)
+                end
+            end
+        end
+
+        @testset "TCP" begin
+            info("TCP Tests")
+
+            @testset "Simple" begin
+                r = tcp_srv(8080)
+                io = Syslog(ip"127.0.0.1", 8080; tcp=true)
+                println(io, "info", "foobar")
+                s = fetch(r)
+                @test contains(s, "foobar")
+                flush(io)
+                close(io)
+            end
+
+            @testset "SysLogHandler" begin
+                @testset "$input" for (level, input, output) in test_logs
+                    r = tcp_srv(8080)
+                    info("Sending (TCP): $level, $input")
+                    io = Syslog(ip"127.0.0.1", 8080; tcp=true)
+                    println(io, level, input)
+                    s = fetch(r)
+                    @test s == output
+                    close(io)
+                end
+            end
+
+            @testset "Disonnect" begin
+                serv = listen(ip"127.0.0.1", 8080)
+                io = Syslog(ip"127.0.0.1", 8080; tcp=true)
+                close(io)
+                log(io, "info", "foobar\n")
+                flush(io)
+                close(io)
+                close(serv)
+            end
+        end
+    end
+
+    @testset "Libc" begin
+        info("Libc Tests")
+        SystemLog.openlog("syslog", 0, SystemLog.LOG_USER)
+        SystemLog.closelog()
+    end
+end
